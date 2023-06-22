779334fcb7902592fd86ac4706e1df16b9720d8f
Deep871xd <102525444+Deep871xd@users.noreply.github.com>
2023-06-22 17:26:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,26 @@
-//your code here
-// let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-// touristSpots.sort((a, b) => {
-//     const articles = /^(The|A|An)\s+/i; 
-
-//     const spotA = a.replace(articles, '');
-//     const spotB = b.replace(articles, '');
-  
-//     return spotA.localeCompare(spotB);
-//   });
-// console.log(touristSpots);
-  
-//  const ulElement = document.getElementById('band');
-
-
-// ulElement.innerHTML = '';
-
-
-// touristSpots.forEach((spot) => {
-//   const liElement = document.createElement('li');
-//   liElement.textContent = spot;
-//   ulElement.appendChild(liElement);
-// });
-
+your code here
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+touristSpots.sort((a, b) => {
+    const articles = /^(The|A|An)\s+/i; 
 
-function removeLeadingArticles(str) {
-  const articles = ['The', 'A', 'An'];
-  const words = str.split(' ');
-  if (articles.includes(words[0])) {
-    return words.slice(1).join(' ');
-  }
-  return str;
-}
+    const spotA = a.replace(articles, '');
+    const spotB = b.replace(articles, '');
+  
+    return spotA.localeCompare(spotB);
+  });
+console.log(touristSpots);
+  
+ const ulElement = document.getElementById('band');
 
-// Sort the tourist spots without leading articles
-touristSpots = touristSpots.sort((a, b) => {
-  const spotA = removeLeadingArticles(a);
-  const spotB = removeLeadingArticles(b);
-  return spotA.localeCompare(spotB);
+
+ulElement.innerHTML = '';
+
+
+touristSpots.forEach((spot) => {
+  const liElement = document.createElement('li');
+  liElement.textContent = spot;
+  ulElement.appendChild(liElement);
 });
+
+
+
